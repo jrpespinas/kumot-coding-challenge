c0789d6713eca1b1ec9b95f26fb23b97a254fe31
Bogs Espinas <jrpespinas@gmail.com>
2022-06-28 23:41:43 +0800

    feat(controller): use logging

--- pkg/http/rest/controller.go
+++ pkg/http/rest/controller.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/jrpespinas/kumot-coding-challenge/pkg/listing"
+	"github.com/rs/zerolog"
 )
 
 // Define an interface of an entry point to our application
@@ -16,25 +17,29 @@ type Controller interface {
 // Define a Controller struct that implements the handler
 type controller struct {
 	service listing.Service
+	logger  zerolog.Logger
 }
 
-// Define a constructor to inject the Service
-func NewController(service listing.Service) Controller {
+// Define a constructor to inject the Service and logger
+func NewController(service listing.Service, logger zerolog.Logger) Controller {
 	return &controller{
 		service: service,
+		logger:  logger,
 	}
 }
 
-// List github username basic details
-// Route: POST /usernames
+// List github user basic details
+// Route: POST /users
 // Access: public
 func (c *controller) ShowDetails(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	// Get POST body
+	c.logger.Info().Str("layer", "controller").Msg("decoding post body")
 	var data Users
 	err := json.NewDecoder(r.Body).Decode(&data)
 	if err != nil {
+		c.logger.Error().Str("layer", "controller").Msgf("error: %v", err.Error())
 		code := http.StatusInternalServerError
 		w.WriteHeader(code)
 		json.NewEncoder(w).Encode(ErrorResponse{
@@ -46,8 +51,10 @@ func (c *controller) ShowDetails(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Validate `data`
+	c.logger.Info().Str("layer", "controller").Msg("validating post body")
 	err = c.service.Validate(data.Usernames)
 	if err != nil {
+		c.logger.Error().Str("layer", "controller").Msgf("error: %v", err.Error())
 		code := http.StatusBadRequest
 		w.WriteHeader(code)
 		json.NewEncoder(w).Encode(ErrorResponse{
@@ -59,8 +66,10 @@ func (c *controller) ShowDetails(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Show details of each username
+	c.logger.Info().Str("layer", "controller").Msg("collecting user details")
 	details, err := c.service.ShowDetails(data.Usernames)
 	if err != nil {
+		c.logger.Error().Str("layer", "controller").Msgf("error: %v", err.Error())
 		code := http.StatusInternalServerError
 		w.WriteHeader(code)
 		json.NewEncoder(w).Encode(ErrorResponse{
@@ -69,6 +78,7 @@ func (c *controller) ShowDetails(w http.ResponseWriter, r *http.Request) {
 			Message: err.Error(),
 		})
 	} else {
+		c.logger.Info().Str("layer", "controller").Msg("displaying user details")
 		code := http.StatusOK
 		w.WriteHeader(code)
 		json.NewEncoder(w).Encode(SuccessResponse{
